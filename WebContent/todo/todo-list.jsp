4f87051ffd1c9b7af2e85fbbcb0b1e40a0923bdd
sungjin <tjdwlsvkdnj2@naver.com>
2022-02-15 01:29:39 +0900

    initt

---
+++ WebContent/todo/todo-list.jsp
@@ -0,0 +1,97 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%> <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="<%=request.getContextPath() %>/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<%=request.getContextPath() %>/css/style.css" />
+    <title>Todo리스트</title>
+  </head>
+  <body>
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: coral">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="<%=request.getContextPath()%>">TODO APP</a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="<%=request.getContextPath()%>/todos?action=list">ToDo 리스트</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav mb-2">
+              <li class="nav-item">
+                <a class="nav-link" href="<%=request.getContextPath()%>/todos?action=logout">로그아웃</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- 네브바 끝 -->
+    <!-- 본문 -->
+    <div class="container mt-5">
+      <h3 class="text-center">할일 목록들</h3>
+      <hr />
+      <div class="container text-left">
+        <a href="<%=request.getContextPath()%>/todos?action=new" class="btn btn-success">할일 추가</a>
+      </div>
+      <br />
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>제목</th>
+            <th>마감 일자</th>
+            <th>현재 상태</th>
+            <th>액션</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- 할일 데이터를 테이블로 -->
+          <c:forEach var="todo" items="${listTodo}">
+            <tr>
+              <td><c:out value="${todo.title}" /></td>
+              <td><c:out value="${todo.targetDate}" /></td>
+              <td class="status"><c:out value="${todo.status}" /></td>
+
+              <td>
+                <a href="<%=request.getContextPath()%>/todos?action=edit&id=<c:out value='${todo.id}'/>" class="btn btn-info btn-sm">수정</a>
+                <a
+                  href="<%=request.getContextPath()%>/todos?action=delete&id=<c:out value='${todo.id}'/>"
+                  onclick="if(!confirm('정말로 삭제하겠습니까?')) return false"
+                  class="btn btn-danger btn-sm"
+                  >삭제</a
+                >
+              </td>
+            </tr>
+          </c:forEach>
+        </tbody>
+      </table>
+      <div class="row mt-5">
+        <div class="col-4 mx-auto">
+          <canvas id="myChart"></canvas>
+        </div>
+      </div>
+    </div>
+    <!-- 본문 끝 -->
+    <jsp:include page="../common/footer.jsp" />
+
+    <script src="<%=request.getContextPath() %>/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="<%=request.getContextPath() %>/js/todo.js"></script>
+  </body>
+</html>
+
+<!-- mx-auto : 가운데 정렬 -->
---
+++ src/todoApp/controller/TodoController.java
@@ -0,0 +1,133 @@
+package todoApp.controller;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import todoApp.dao.TodoDao;
+import todoApp.dao.TodoDaoImpl;
+import todoApp.model.Todo;
+
+
+// 서블릿이 기본"/"주소이면 다른 서블릿 "/register , /login" 등을 제외한 모든 요청이 여기에서 처리 
+@WebServlet("/todos")
+public class TodoController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private TodoDao todoDAO;
+	
+	public void init() {
+		todoDAO = new TodoDaoImpl(); //실제 객체는 todoDao를 구현한 TodoDaoImple로 생성
+	}
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response); //post로 요청하더라도 get으로 처리
+	}
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//요청주소가 localhost:8090/TODO/new => "/new" 가 action의 값
+		String action = request.getParameter("action");
+		System.out.println(action);
+		switch(action) {
+		case "new":
+			showNewForm(request, response);
+			break;
+		case "post":
+			insertTodo(request, response);
+			break;	
+		case "delete":
+			deleteTodo(request, response);
+			break;
+		case "edit": // 수정 폼을 보여줌
+			showEditForm(request, response);
+			break;
+		case "update":
+			updateTodo(request, response);
+			break;
+		case "list": //localhost:8090/TODO/todos/list
+			listTodo(request, response);
+			break;
+		default:	//요청주소가 기본 또는 잘못되었을 경우 로그인 페이지로 이동
+			HttpSession session = request.getSession();
+			session.invalidate(); //로그인 정보를 모두 삭제
+			RequestDispatcher dispatcher = request.getRequestDispatcher("login/login.jsp");
+			dispatcher.forward(request, response);		
+		} //스위치 문 끝
+		
+	}
+
+	private void updateTodo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+		// 업데이트시에는 id도 입력됨
+		Long id = Long.parseLong(request.getParameter("id"));	
+		String title = request.getParameter("title");
+		String username = (String)session.getAttribute("username");
+		String description = request.getParameter("description");
+		LocalDate targetDate = LocalDate.parse(request.getParameter("targetDate"));		
+		boolean isDone = Boolean.valueOf(request.getParameter("isDone"));
+		
+		Todo todo = new Todo(id, title, username, description, targetDate, isDone);		
+		todoDAO.updateTodo(todo);
+		
+		response.sendRedirect("todos?action=list");	// 할일을 업데이트한 후 리스트 페이지로 이동		
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Long id = Long.parseLong(request.getParameter("id")); //id를 받음
+		Todo theTodo = todoDAO.selectTodo(id);
+		// 수정할 todo객체를 같이 보냄
+		request.setAttribute("todo", theTodo);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("todo/todo-form.jsp");
+		dispatcher.forward(request, response);	
+		
+	}
+
+	private void deleteTodo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Long id = Long.parseLong(request.getParameter("id")); //id를 받음
+		todoDAO.deleteTodo(id);
+		
+		response.sendRedirect("todos?action=list");	// 삭제 후 리스트 페이지로 이동
+	}
+
+	private void insertTodo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		HttpSession session = request.getSession();
+			
+		String title = request.getParameter("title");
+		String username = (String)session.getAttribute("username");
+		String description = request.getParameter("description");
+		LocalDate targetDate = LocalDate.parse(request.getParameter("targetDate"));		
+		boolean isDone = Boolean.valueOf(request.getParameter("isDone"));
+		
+		Todo newTodo = new Todo(title, username, description, targetDate, isDone);		
+		todoDAO.insertTodo(newTodo);
+		
+		response.sendRedirect("todos?action=list");	// 새 할일을 저장후에 리스트 페이지로 이동	
+	}
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("todo/todo-form.jsp");
+		dispatcher.forward(request, response);			
+	}
+
+	private void listTodo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Todo> listTodo = todoDAO.selectAllTodos(); //DB에서 할일들을 가져와 리스트에 저장
+		request.setAttribute("listTodo", listTodo);	    //리스트를 리퀘스트에 저장
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher("todo/todo-list.jsp");
+		dispatcher.forward(request, response);		
+	}
+
+}
